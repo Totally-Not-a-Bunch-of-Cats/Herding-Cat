74f3a3dd5b8a4bdb6a480f5c86a8ff795906cb9d
superbird29 <aaron.tweden2014@gmail.com>
2022-05-13 00:58:43 -0500

    pushing the build I am doing stuff on

---
+++ Assets/Catmove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catmove : MonoBehaviour
+{
+    void Update()
+    {
+      
+    }
+
+    /// <summary>
+    /// moves the cat to the left a number of tiles equal to num
+    /// </summary>
+    /// <param name="Num"></param>
+    void MoveLeft(int Num)
+    {
+
+    }
+
+    void MoveRight(int Num)
+    {
+
+    }
+
+    void MoveUp(int Num)
+    {
+
+    }
+
+    void MoveDown(int Num)
+    {
+
+    }
+}
