08b08d07a35d4e8269e4c2a49013c4f171ae76c1
linkd5625 <70868908+linkd5625@users.noreply.github.com>
2022-06-18 20:02:58 -0500

    stuff

--- Assets/Script/Managers/RoundManager.cs
+++ Assets/Script/Managers/RoundManager.cs
@@ -15,6 +15,7 @@ public class RoundManager : MonoBehaviour
     // Varables
     //-------------------------------------------
 
+    private enums.spotType[,] board;
     private Vector2Int BoardSize;
     private int ItemPlaced = 0;
     private int RoundPassed = 0;
@@ -22,6 +23,7 @@ public class RoundManager : MonoBehaviour
     //-------------------------------------------
     // Functions
     //-------------------------------------------
+
     /// <summary>
     /// is triggured when the player hits the end turn button. And triggures all of the chained items
     /// </summary>
---
+++ Assets/Script/enums.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class enums
+{
+
+   public enum spotType { Cat, Item, Blank, Obsticle };
+
+}
