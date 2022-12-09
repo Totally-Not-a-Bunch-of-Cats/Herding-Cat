6651e3fa678f3811d053927b78f87e2d5fb074bf
linkd5625 <70868908+linkd5625@users.noreply.github.com>
2022-12-08 18:31:42 -0600

    Got scaling of board working
    Sets cameras orthographic size to make board take up most space vertically

--- Assets/Script/Managers/ScreenResizeManager.cs
+++ Assets/Script/Managers/ScreenResizeManager.cs
@@ -8,12 +8,15 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class ScreenResizeManager : MonoBehaviour
 {
+    
     public enum Rotation {Landscape, Portrait }
 
+    // Rotation of the device
     public Rotation CurrentRotation;
-    public float TileSize;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Starts the script
+    /// </summary>
     void Start()
     {
         if (Screen.height > Screen.width)
@@ -25,22 +28,12 @@ public class ScreenResizeManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Sets orthographic size of camera to the size requried to make board fill screen
+    /// </summary>
     public void ScaleBoard ()
     {
-        Debug.Log(CurrentRotation);
-        if (CurrentRotation == Rotation.Landscape)
-        {
-            TileSize = Screen.height / (GameManager.Instance._matchManager.BoardSize[0] + 1);
-        } else
-        {
-            TileSize = Screen.width / (GameManager.Instance._matchManager.BoardSize[1] + 1);
-        }
-        GameManager.Instance._matchManager.BoardTileMap.GetComponentInParent<Grid>().cellSize = new Vector3(TileSize, TileSize, 1);
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
+        float OrthoSize = (GameManager.Instance._matchManager.BoardSize[1] + 1f) * 0.5f;
+        Camera.main.orthographicSize = OrthoSize;
     }
 }
