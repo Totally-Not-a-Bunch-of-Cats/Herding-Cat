3ae4114f7b14c2e01b64b935460bc0e28cdb2623
linkd5625 <70868908+linkd5625@users.noreply.github.com>
2023-01-04 21:10:21 -0600

    worked on level design tool
    Did some refactoring of the new board size, added the generation of tile map for the board

--- Assets/Script/LevelCreationTool.cs
+++ Assets/Script/LevelCreationTool.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
-public class LevelCreationTool: MonoBehaviour
+public class LevelCreationTool : MonoBehaviour
 {
-    public int SizeX;
-    public int SizeY;
+    public Vector2Int BoardSize;
     public string LevelName;
-        
+    public UnityEngine.Tilemaps.Tile BackgroundTile;
+
     //then activates buttons to begin working on the level
 
     /// <summary>
@@ -19,7 +20,7 @@ public class LevelCreationTool: MonoBehaviour
     {
         if (int.Parse(X) > 0)
         {
-            SizeX = int.Parse(X);
+            BoardSize.x = int.Parse(X);
         }
     }
     /// <summary>
@@ -30,7 +31,7 @@ public class LevelCreationTool: MonoBehaviour
     {
         if (int.Parse(Y) > 0)
         {
-            SizeY = int.Parse(Y);
+            BoardSize.y = int.Parse(Y);
         }
     }
     /// <summary>
@@ -45,13 +46,16 @@ public class LevelCreationTool: MonoBehaviour
 
     public void Continue()
     {
-        if(LevelName != null && SizeX != 0 && SizeY != 0)
+        if (LevelName != null && BoardSize.x != 0 && BoardSize.y != 0)
         {
             Debug.Log("herlp");
+            //turn off level info thing 
+            GameObject.Find("Level Set Up").SetActive(false);
+            //then make blank of proper size
+            GenerateBlankBoard();
+            //then turn on buttons with items
+
         }
-        //turn off level info thing 
-        //then make blank of proper size
-        //then turn on buttons with items
     }
 
     /// <summary>
@@ -60,5 +64,43 @@ public class LevelCreationTool: MonoBehaviour
     public void Save()
     {
 
+    }
+
+    /// <summary>
+    /// Generates a blank tilemap of the new board size
+    /// </summary>
+    public void GenerateBlankBoard()
+    {
+        Vector3 BoardOffset = new Vector3();
+        Tilemap BoardTileMap = GameObject.Find("Tilemap").GetComponent<Tilemap>();
+        GameObject.Find("OutlineSquare").transform.localScale = new Vector3(BoardSize.x, BoardSize.y, 1);
+        // Generates grid (Odd numbered sizes will break)
+        int tempx = BoardSize.x / 2;
+        int tempy = BoardSize.y / 2;
+        bool oddX = BoardSize.x % 2 != 0;
+        bool oddY = BoardSize.y % 2 != 0;
+
+        if (oddX)
+        {
+            BoardOffset.x = 0.5f;
+        }
+        if (oddY)
+        {
+            BoardOffset.y = 0.5f;
+        }
+
+        if (oddX || oddY)
+        {
+            BoardTileMap.transform.localPosition += BoardOffset;
+        }
+
+        // setup background(tilemap)
+        for (int x = (int)(-tempx - (0.5f + BoardOffset.y)); x < tempx; x++)
+        {
+            for (int y = (int)(-tempy - (0.5f + BoardOffset.y)); y < tempy; y++)
+            {
+                BoardTileMap.SetTile(new Vector3Int(x, y, 0), BackgroundTile);
+            }
+        }
     }
 }
