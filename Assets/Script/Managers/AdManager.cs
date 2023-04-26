de95430b8ffee725c357a9dbd3810a7dce32b49f
superbird29 <aaron.tweden2014@gmail.com>
2023-04-25 23:08:59 -0500

    trying to do ads
    and failing

--- Assets/Script/Managers/AdManager.cs
+++ Assets/Script/Managers/AdManager.cs
@@ -5,11 +5,22 @@ public class AdManager : MonoBehaviour
 {
 #if UNITY_IOS
     string GameID = "5257934";
-#else
+#elif UNITY_ANDROID
     string GameID = "5257935";
 #endif
+    [SerializeField] bool _testMode = true;
     void Start()
     {
-        //InitializeAds();
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+#if UNITY_IOS || UNITY_ANDROID
+        if (!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Advertisement.Initialize(GameID, _testMode, this);
+        }
+#endif
     }
 }
