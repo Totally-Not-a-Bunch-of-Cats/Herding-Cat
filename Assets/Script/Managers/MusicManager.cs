a1a6b75521edaf71dbec0fe0962e5e6b4f0c5744
superbird29 <aaron.tweden2014@gmail.com>
2023-10-01 21:53:37 -0500

    working on sound stuff

---
+++ Assets/Script/Managers/MusicManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource AudioPlayer;
+    public List<AudioClip> AudioTracks;
+    public List<AudioClip> AudioEffect;
+
+    public void StartTrack(int location)
+    {
+        AudioPlayer.clip = AudioEffect[location];
+        AudioPlayer.Play();
+    }
+    public void PlayAudioEffect(int location)
+    {
+        AudioPlayer.clip = AudioEffect[location];
+        AudioPlayer.Play();
+    }
+}
